9e8989f365faf3e621bce611776772aa501fa5cc
Neeraj Kashyap <nkashyap@adaptix.systems>
2015-05-16 16:53:43 -0700

    Passing database configuration test.
    Test wasn't functioning correctly before because I wasn't using specs2 to deal with Future objects correctly.
    
    Still am not completely satisfied with the structure of DatabaseConfigSpecification. Could do with a test for the table drop that takes place in the afterAll method. But it's time to move on to actual bling functionality. The test improvement can come later.

--- src/test/scala/systems/adaptix/bling/data/DatabaseConfigSpecification.scala
+++ src/test/scala/systems/adaptix/bling/data/DatabaseConfigSpecification.scala
@@ -1,17 +1,64 @@
 package systems.adaptix.bling.data
 
 import org.specs2.mutable._
+import org.specs2.specification._
+import org.specs2.concurrent.ExecutionEnv
 
-import com.typesafe.config._
-import slick.jdbc.JdbcBackend._
-import slick.driver._
+import slick.driver.PostgresDriver.api._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent._
+import scala.concurrent.duration._
+import slick.dbio.DBIO
+import slick.lifted.{Tag, TableQuery}
 
 /**
  * Created by nkashyap on 5/15/15.
  */
-class DatabaseConfigSpecification extends Specification {
-  "Connect to database specified by \"blingtestdb\" in configuration." >> {
-    val db = Database.forConfig("blingtestdb")
-    true
+class DatabaseConfigSpecification extends Specification with AfterAll {
+  val db = Database.forConfig("blingtestdb")
+
+  case class TestDatum(key: Option[Int], value: String)
+  class Test(tag: Tag) extends Table[TestDatum](tag, "TEST") {
+    def key = column[Int]("KEY", O.PrimaryKey, O.AutoInc)
+    def value = column[String]("VALUE")
+
+    def * = (key.?, value) <> (TestDatum.tupled, TestDatum.unapply)
+  }
+  val test = TableQuery[Test]
+  val setup = DBIO.seq(
+    test.schema.create
+  )
+  db.run(setup)
+
+  def afterAll = {
+    db.run(test.schema.drop)
+    db.close()
+  }
+
+  "bling should be able to connect to a database, create a table, insert some values, and make a query." >> {implicit ee: ExecutionEnv =>
+    val entries = Seq(
+      TestDatum(None, "Alice"),
+      TestDatum(None, "Bob"),
+      TestDatum(None, "Carol"),
+      TestDatum(None, "Dave"),
+      TestDatum(None, "Edgar")
+    )
+
+    val insertions = DBIO.seq(
+      test ++= entries
+    )
+    val selectQuery = for(pair <- test) yield pair
+
+    val insertion = db.run(insertions)
+    val selection = insertion flatMap {_ => db.run(selectQuery.result)}
+    val expectedResult = Vector(
+      TestDatum(Some(1), "Alice"),
+      TestDatum(Some(2), "Bob"),
+      TestDatum(Some(3), "Carol"),
+      TestDatum(Some(4), "Dave"),
+      TestDatum(Some(5), "Edgar")
+    )
+
+    selection must be_==(expectedResult).await
   }
 }
