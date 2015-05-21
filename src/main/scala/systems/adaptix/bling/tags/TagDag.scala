946c5ccab67b00bbaa15cdd3bcc9111f24c08f68
Neeraj Kashyap <nkashyap@adaptix.systems>
2015-05-21 15:40:51 -0700

    insertTag method of TagDag updated.

--- src/main/scala/systems/adaptix/bling/tags/TagDag.scala
+++ src/main/scala/systems/adaptix/bling/tags/TagDag.scala
@@ -7,9 +7,21 @@ import scala.collection.mutable
  * Created by nkashyap on 5/18/15.
  */
 class TagDag(val universalTag: String) extends RootedDag(DagVertex(universalTag)) {
-  val allTags = mutable.Set[String](universalTag)
-  def hasTag(tag: String) = allTags contains tag
-  def insertTag(tag: String) = {
-    root addChild DagVertex(tag)
+  val allTags = mutable.Map[String, DagVertex](universalTag -> root)
+
+  def hasTag(tag: String) = allTags.keys exists(_ == tag)
+
+  def insertTag(tag: String, parents: Set[String] = Set(universalTag)) = {
+    if (hasTag(tag)) {
+      throw new IllegalArgumentException("The attempted insertion did not preserve uniqueness of tags.")
+    }
+    val tagVertex = DagVertex(tag)
+    parents map (allTags(_) addChild tagVertex)
+    if (isAcyclic) {
+      allTags += (tag -> tagVertex)
+    } else {
+      parents map (allTags(_) removeChild tagVertex)
+      throw new IllegalArgumentException("The attempted insertion did not preserve acyclicity: " + tag)
+    }
   }
 }
--- src/test/scala/systems/adaptix/bling/tags/TagDagSpecification.scala
+++ src/test/scala/systems/adaptix/bling/tags/TagDagSpecification.scala
@@ -18,13 +18,13 @@ class TagDagSpecification extends Specification {
   }
 
   "TagDags build upon RootedDags by providing a guarantee that each string occurs AT MOST ONCE as a vertex label. To this end, a TagDag provides an interface to vertex and label manipulation." >> {
-    "The labels of the vertices of a TagDag are stored in its allTags member value, which is of type mutable.Set." >> {
+    "The labels of the vertices of a TagDag are stored as keys in its allTags member value, which is a mutable mapping from Strings to DagVertices. The value matching each of these tags is the DagVertex in the TagDag which has that tag as its label." >> {
       val tags = new TagDag("lol")
       tags.allTags must haveSize(1)
-      tags.allTags must contain("lol")
+      tags.allTags.getOrElse("lol", "") mustEqual tags.root
     }
 
-    "The \"hasTag\" method is a means of testing whether or not a TagDag already contains a given tag." >> {
+    "The \"hasTag\" method is a means of testing whether or not a TagDag already contains a vertex with a given tag." >> {
       val tags = new TagDag("lol")
       tags hasTag("lol") must beTrue
       tags hasTag("rofl") must beFalse
@@ -36,7 +36,29 @@ class TagDagSpecification extends Specification {
         tags.root.children must beEmpty
         tags.insertTag("new")
         tags.root.children must haveSize(1)
-        tags.root.children.map(_.label == "new") mustEqual mutable.Set(true)
+        tags.root.children must contain(tags allTags("new"))
+      }
+
+      "The method also allows the insertion of a tag as a child to a specified Set of parents." >> {
+        val tags = new TagDag("root")
+        tags.root.children must beEmpty
+        tags.insertTag("child1")
+        tags.insertTag("child2")
+        tags.insertTag("grandchild", Set("child1", "child2"))
+
+        val rootChildren = (tags allTags "root").children
+        rootChildren must haveSize(2)
+        rootChildren must contain(tags allTags "child1")
+        rootChildren must contain(tags allTags "child2")
+        rootChildren must not contain(tags allTags "grandchild")
+
+        val child1Children = (tags allTags "child1").children
+        child1Children must haveSize(1)
+        child1Children must contain(tags allTags "grandchild")
+
+        val child2Children = (tags allTags "child2").children
+        child2Children must haveSize(1)
+        child2Children must contain(tags allTags "grandchild")
       }
     }
   }
