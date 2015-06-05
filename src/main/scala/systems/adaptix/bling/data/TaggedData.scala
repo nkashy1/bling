f588dd1a00a1d2dc65c3c7252d11c4f3adaff034
Neeraj Kashyap <nkashyap@adaptix.systems>
2015-06-05 15:08:42 -0700

    no vals needed in case classes

--- src/main/scala/systems/adaptix/bling/data/TaggedData.scala
+++ src/main/scala/systems/adaptix/bling/data/TaggedData.scala
@@ -3,4 +3,4 @@ package systems.adaptix.bling.data
 /**
  * Created by nkashyap on 6/5/15.
  */
-case class TaggedData(val data: Map[String, Any], val tags: Set[String])
+case class TaggedData(data: Map[String, Any], tags: Set[String])
