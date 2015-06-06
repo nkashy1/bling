b1666be690bac50104fcbf039bda857e9236fa45
Neeraj Kashyap <nkashyap@adaptix.systems>
2015-06-05 21:40:09 -0700

    Made a very decent push on the Absorber. Have to write more tests before I can resolve the tag-based insertion task, and move onto tag-based querying!

--- src/main/scala/systems/adaptix/bling/data/Absorber.scala
+++ src/main/scala/systems/adaptix/bling/data/Absorber.scala
@@ -25,30 +25,42 @@ class Absorber(val dataTemplate: TableTemplate, val tagsTemplate: TagTableTempla
   createTableIfItDoesNotExist(tagsTemplate)
 
   private def loadTagIndexers: Map[String, IdTableTemplate] = {
-    val existingTags = sql"SELECT * FROM ${tagsTemplate.sqlTableName}".list.apply()
+    val existingTags = sql"SELECT * FROM ${tagsTemplate.sqlTableName}".map(_.toMap).list.apply().map(_.head._2.toString)
     Map[String, IdTableTemplate](existingTags zip existingTags.map( tag => new IdTableTemplate(tag, "id") ) :_*)
   }
-  var tagIndexers = loadTagIndexers
+  var tagIndexers: Map[String, IdTableTemplate] = loadTagIndexers
   tagIndexers.foreach( pair => createTableIfItDoesNotExist(pair._2) )
 
-  def validateInput(input: TaggedData): Boolean = {
-    (input.tags subsetOf columnNameSet) && (requiredFieldNameSet subsetOf input.tags)
+  def absorb(input: TaggedData) = {
+    val rowId = insertDataAndGetId(input.data)
+    val relevantTagIndexers = input.tags.map(getIdTableTemplate)
+    relevantTagIndexers.foreach( indexer => indexId(rowId, indexer) )
   }
 
-  def insertDataAndGetId(input: TaggedData): Long = {
-    val columnsString = input.data.keys.map( key => s"${key}" ).mkString(", ")
-    val valuesPlaceHoldersString = input.data.keys.map ( _ => "?" ).mkString(", ")
-    val insertionString = s"INSERT INTO ${dataTemplate.tableName} (${columnsString}) VALUES (${valuesPlaceHoldersString})"
-    sql"${insertionString}".bind(input.data.values.toSeq:_*).updateAndReturnGeneratedKey().apply()
+  private def insertDataAndGetId(data: Map[String, Any]): Long = {
+    val columnsSql = SQLSyntax.createUnsafely( data.keys.map( key => s"${key}" ).mkString(", ") )
+    val valuesPlaceHolderSql = SQLSyntax.createUnsafely( Seq.fill(data.keys.size)("?").mkString(", ") )
+    sql"INSERT INTO ${dataTemplate.sqlTableName} (${columnsSql}) VALUES (${valuesPlaceHolderSql})".
+      bind(data.values.toList:_*).updateAndReturnGeneratedKey().apply()
   }
 
-  def getIdTableTemplate(tag: String) = {
+  private def getIdTableTemplate(tag: String) = {
     tagIndexers.getOrElse(tag, registerNewTag(tag))
   }
 
-  def registerNewTag(tag: String) = {
+  private def registerNewTag(tag: String) = {
     val tagIndexer = new IdTableTemplate(tag, "id")
+    createTableIfItDoesNotExist(tagIndexer)
     tagIndexers = tagIndexers + (tag -> tagIndexer)
     tagIndexer
+  }
+
+  private def indexId(id: Long, tagIndexer: IdTableTemplate) = {
+    val columnSql = SQLSyntax.createUnsafely(tagIndexer.columnName)
+    sql"INSERT INTO ${tagIndexer.sqlTableName} (${columnSql}) VALUES (?)".bind(id).update().apply()
+  }
+
+  def validateFields(input: TaggedData): Boolean = {
+    (input.data.keySet subsetOf columnNameSet) && (requiredFieldNameSet subsetOf input.data.keySet)
   }
 }
--- src/test/scala/systems/adaptix/bling/data/AbsorberSpecification.scala
+++ src/test/scala/systems/adaptix/bling/data/AbsorberSpecification.scala
@@ -1,29 +1,60 @@
 package systems.adaptix.bling.data
 
+import org.joda.time.DateTime
 import org.specs2.mutable.Specification
+
+import scalikejdbc._
+import scalikejdbc.config._
 
 /**
  * Created by nkashyap on 6/5/15.
  */
 class AbsorberSpecification extends Specification {
+  sequential
+
+  DBs.setupAll()
+  implicit val session = AutoSession
+
+  val id = AutoIdFieldInfo("id")
+  val name = PlainFieldInfo("name", "VARCHAR")
+  val number = PlainFieldInfo("random", "INT")
+  val dataTemplate = new TableTemplate("AbsorberSpecification_data", Seq(id, name, number))
+
+  val tag = PrimaryFieldInfo("tag", "VARCHAR")
+  val tagsTemplate = new TagTableTemplate("AbsorberSpecification_tags", "tag")
+
+  val absorber = new Absorber(dataTemplate, tagsTemplate)
+
   "The Absorber class allows TaggedData to be loaded into the table specified by a given TableTemplate in a database, with the tags being registered in a table specified by a TagTableTemplate." >> {
-    val id = AutoIdFieldInfo("id")
-    val name = PlainFieldInfo("name", "VARCHAR")
-    val ts = NotNullFieldInfo("stamp", "TIMESTAMP")
-    val dataTemplate = new TableTemplate("AbsorberSpecification_data", Seq(id, name, ts))
-
-    val tag = PrimaryFieldInfo("tag", "VARCHAR")
-    val tagsTemplate = new TagTableTemplate("AbsorberSpecification_tags", "tag")
-
-    val absorber = new Absorber(dataTemplate, tagsTemplate)
-
     "The dataTemplate and tagsTemplate TableTemplates specifying the schema of the data table and the tags table respectively are passed at instantiation." >> {
       absorber.dataTemplate mustEqual dataTemplate
       absorber.tagsTemplate mustEqual tagsTemplate
     }
 
-    "The absorb method." >> {
-      pending
+    "With a fresh database, the Absorber object's tagIndexers method should be empty." >> {
+      absorber.tagIndexers must beEmpty
     }
+
+    "The Absorber's functionality is exposed via the absorb method." >> {
+      val input1 = TaggedData(Map[String, Any]("name" -> "bob"), Set("lol"))
+      absorber.absorb(input1)
+      absorber.tagIndexers.keySet must contain("lol")
+      sql"SELECT id FROM ${absorber.tagIndexers("lol").sqlTableName}".map(_.toMap).first.apply() mustEqual Some(Map[String, Any]("id".toUpperCase -> 1))
+    }
+
+    "validateFields is a utility method that can be used to check if a given TaggedData object is appropriate for absorption in terms of the fields that it inserts values into." >> {
+      val validInput = TaggedData(Map[String, Any]("name" -> "bob", "random" -> 42), Set("lol"))
+      absorber.validateFields(validInput) must beTrue
+
+      val invalidInput = TaggedData(Map[String, Any]("lol" -> "rofl", "name" -> "divid"), Set("rofl"))
+      absorber.validateFields(invalidInput) must beFalse
+    }
+  }
+
+  "Cleaning up." >> {
+    sql"${dataTemplate.sqlDrop}".execute.apply()
+    sql"${tagsTemplate.sqlDrop}".execute.apply()
+    absorber.tagIndexers.foreach(pair => sql"${pair._2.sqlDrop}".execute.apply())
+    ok
   }
 }
