0c87516b824c02547be1111440a915b3b6e89727
Neeraj Kashyap <nkashyap@adaptix.systems>
2015-06-05 17:37:46 -0700

    Fixued TagTableTemplate constructor

--- src/main/scala/systems/adaptix/bling/data/TagTableTemplate.scala
+++ src/main/scala/systems/adaptix/bling/data/TagTableTemplate.scala
@@ -3,4 +3,4 @@ package systems.adaptix.bling.data
 /**
  * Created by nkashyap on 6/5/15.
  */
-class TagTableTemplate(override val tableName: String, val columnName: String) extends TableTemplate(tableName, Seq( PrimaryFieldInfo(columnName, "VARCHAR") ))
+class TagTableTemplate(tableName: String, val columnName: String) extends TableTemplate(tableName, Seq( PrimaryFieldInfo(columnName, "VARCHAR") ))
