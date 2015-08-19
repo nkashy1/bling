986b35f89f8ef16eb9f3927fc615b8ca55678bae
Neeraj Kashyap <nkashy1@gmail.com>
2015-08-19 12:06:54 -0700

    fixed up scaladoc formatting for BlingConsole.

--- src/main/scala/systems/adaptix/bling/BlingConsole.scala
+++ src/main/scala/systems/adaptix/bling/BlingConsole.scala
@@ -11,15 +11,25 @@ import scala.io.Source
  * at the time of instantiation. Various factory objects can be created which perform the appropriate mix-ins and specifications at run-time.
  *
  * The following is a list of the information that needs to be provided to a BlingConsole at instantiation:
+ *
  * 1. A [[systems.adaptix.bling.tags.TagDagSerializer]] to be mixed in, which specifies the format in which the console's member [[systems.adaptix.bling.tags.TagDag]] is to be serialized.
+ *
  * 2. The RawData abstract type to be specified, which specifies the format in which raw data will be presented to bling.
+ *
  * 3. A convertToTaggedData abstract method to be implemented, which converts RawData into TaggedData.
+ *
  * 4. A BlingData abstract type to be specified, which specifies the format in which bling is expected to present data to its users.
+ *
  * 5. A convertToBlingData abstract method to be implemented, which converts key-value pairs of data extracted from the database into BlingData.
+ *
  * 6. An implicit scalikejdbc.DBSession to be defined in the scope of instantiation.
+ *
  * 7. A dataHandler abstract value to be specified, which is an object of type derived from [[systems.adaptix.bling.data.DataHandler]].
+ *
  * 8. A tagDag abstract variable to be specified, which is an object of type derived from [[systems.adaptix.bling.tags.TagDag]].
+ *
  * 9. A tagDagFileName abstract value to be specified, which is a String containing the location of a file on the filesystem which is meant to store an up-to-date copy of the serialization of the tagDag member.
+ *
  * 10. A blingId abstract value to be specified, which is a String containing the name of the internal identification column for data in the bling data store.
  *
  * Created by nkashyap on 6/10/15.
